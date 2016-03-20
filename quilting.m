fef5f3737a80b3cf9ccc6dc63b9c7738cd163683
Jordan Mecom <jmecom@wustl.edu>
2016-03-20 18:02:24 -0500

    Fixed bug, working on ssd matches

--- quilting.m
+++ quilting.m
@@ -1,68 +1,73 @@
 % Parameters
-bsize = 48;             % Set by user
+bsize = 36;             % Set by user
 ovsize = bsize/6;       % Efros and Freeman
-k = 5;                 % Tolerance
+k = 7;                  % Tolerance
 
 % Open image
-im = imread('images/FarmAerial.jpg');
+im = double(imread('images/FarmAerial.jpg'));
 [im_h, im_w, ~] = size(im);
 
-mri = floor(im_w/bsize) - 1; % maximum row index
-mci = floor(im_h/bsize) - 1; % maximum col index
+mri = floor(im_h/bsize); % maximum row index
+mci = floor(im_w/bsize); % maximum col index
 
 out = uint8(zeros(im_w*2, im_h*2, 3));
 [out_h, out_w, ~] = size(out);
 
-% Set the first block at random
-out(1:bsize, 1:bsize, :) = get_block(im, bsize, randi(mci), randi(mri));
+% Set the first block at random for the block comparison
+prev_block = get_block(im, bsize, randi(mri), randi(mci));
+out(1:bsize, 1:bsize, :) = prev_block;
 
 % Fill in first row
-for col = 1:(out_w/bsize)+2
-  col_cut = col*bsize:(col+1)*bsize;
+for col = 2:floor((out_w/bsize)) + 1    % +1 fills in gaps at end
+  col_cut = max(1, (col-1)*bsize):col*bsize;
   
   if length(col_cut) > bsize
     col_cut = col_cut(1:end-1);
-  end
-
-  % Totally random selection
-%   out(1:bsize, col_cut, :) = get_block(im, bsize, randi(mci), randi(mri));
-
-  % Random selection from k best matches
-  comp_col_cut = col_cut - bsize;
-  if col == 1
-    comp_col_cut = col_cut - bsize+1;
-  end
+  end 
+ 
+     %   Totally random selection
+%   out(1:bsize, col_cut, :) = get_block(im, bsize, randi(mri), randi(mci));
   
-  comp = out(1:bsize, comp_col_cut, :);
-  out(1:bsize, col_cut, :) = random_match(comp, im, bsize, ovsize, k);
+  
+  %   Random selection from k best matches
+%   comp_col_cut = col_cut - bsize;
+%   if col == 1
+%     comp_col_cut = col_cut - bsize+1;
+%   end
+  
+%   comp = out(1:bsize, comp_col_cut, :);
+  
+  prev_block = random_match(prev_block, im, bsize, ovsize, k);
+  out(1:bsize, col_cut, :) = prev_block;
 end
-% 
-% Fill in first column
+
+%
+% % Fill in first column
 % for row = 1:(out_h/bsize)+2            % TODO: always +2 ? edge case?
 %   row_cut = row*bsize:(row+1)*bsize;
-%   
+%
 %   if length(row_cut) > bsize
 %     row_cut = row_cut(1:end-1);
 %   end
-%   
+%
 %   out(row_cut, 1:bsize, :) = get_block(im, bsize, randi(mci), randi(mri));
 % end
-% 
+%
 % % Fill in the rest
 % for row = 1:(out_h/bsize)+2
 %   row_cut = row*bsize:(row+1)*bsize;
-%   
+%
 %   if length(row_cut) > bsize
 %     row_cut = row_cut(1:end-1);
 %   end
-%   
+%
 %   for col = 1:(out_w/bsize)+2
 %     col_cut = col*bsize:(col+1)*bsize;
-%     
+%
 %     if length(col_cut) > bsize
 %       col_cut = col_cut(1:end-1);
 %     end
-%     
+%
 %     % Randomly pick blocks
 %     out(row_cut, col_cut, :) = get_block(im, bsize, randi(mci), randi(mri));
 %   end
--- random_match.m
+++ random_match.m
@@ -4,54 +4,27 @@ function [ match ] = random_match( comp, im, bsize, ovsize, k )
 [height, width, ~] = size(im);   
 matches = [];
 
-% 
-x = 1;
-while x <= width - bsize
-  y = 1;
-  
-  x_cut = x:x+bsize;
-  if length(x_cut) > bsize
-    x_cut = x_cut(1:end-1);
+cols = floor(width / bsize);
+rows = floor(height / bsize);
+for c = 1 : cols
+  for r = 1 : rows
+    block = get_block(im, bsize, r, c);
+    
+    % Get the overlap region
+    comp_ov = comp(:, (bsize - ovsize + 1):bsize, :); % Left image's overlap
+    block_ov = block(:, 1:ovsize, :);                 % Right image's overlap
+    
+    ssd = sum((comp_ov(:) - block_ov(:)).^2)
+    matches = [matches [r; c; ssd]]; % appending the match
   end
-  
-  while y <= height - bsize
-    
-    y_cut = y:y+bsize;
-    if length(y_cut ) > bsize
-      y_cut = y_cut(1:end-1);
-    end
-    
-    b = im(y_cut, x_cut, :);
-%     imshow(b);
-    
-    comp_ov = comp(:, (bsize - ovsize):bsize, :);
-    b_ov = b(:, 1:ovsize+1, :);
-
-    
-    ssd = sum((comp_ov(:) - b_ov(:)).^2);
-    matches = [matches [y; x; ssd]];
-    y = y + bsize;
-  end
-  x = x + bsize;
 end
 
+% Sort the possible matches by the errors
 matches = sortrows(matches', 3)';
-matches = matches(:, 1:k);
-match_indices = matches(:, length(matches));
-y = match_indices(1, :);
-x = match_indices(2, :);
 
-x_cut = x:x+bsize;
-if length(x_cut) > bsize
-  x_cut = x_cut(1:end-1);
-end
-
-y_cut = y:y+bsize;
-if length(y_cut) > bsize
-  y_cut = y_cut(1:end-1);
-end
-
-match = im(y_cut, x_cut, :);
+% Choose random match from the top k matches
+match_index = matches(:, randi(k));
+match = get_block(im, bsize, match_index(1), match_index(2));
 
 end
 
